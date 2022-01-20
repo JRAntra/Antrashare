9e49e8fc63f0cc4ad0bda9c2ef817b2f740cf138
Julian Cai <zcai57@wisc.edu>
2022-01-20 16:40:24 -0600

    create loginpage loginform components

--- Antrashare_ui/src/app/app.module.ts
+++ Antrashare_ui/src/app/app.module.ts
@@ -9,10 +9,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginFormComponent } from './login-page/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
