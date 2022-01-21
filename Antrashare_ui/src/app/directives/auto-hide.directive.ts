6b572f5acd83f28c91b813fed8de8f74e9d2caee
porry <porrychen@gmail.com>
2022-01-20 22:51:18 -0600

    changed attribute directive to structural directive

--- Antrashare_ui/src/app/directives/auto-hide.directive.ts
+++ Antrashare_ui/src/app/directives/auto-hide.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, TemplateRef, Input, OnInit, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[autoHide]'
 })
 export class AutoHideDirective implements OnInit {
+  time: number = DEFAULT_TIME;
 
-  constructor(private el: ElementRef) { }
+  constructor(private templateRef: TemplateRef<any>, private viewRef: ViewContainerRef) { }
 
   ngOnInit(): void {
-      this.el.nativeElement.style.backgroundColor = 'red';
+    // display the template
+    this.viewRef.createEmbeddedView(this.templateRef);
+    // set a time to hide the view
+    setTimeout(() => {
+      this.viewRef.clear();
+    }, this.time);
   }
 }
+
+export const DEFAULT_TIME: number = 5000;
