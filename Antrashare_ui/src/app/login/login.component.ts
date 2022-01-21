5dd461f828cc7de5b5ecd469d4fb6834e4399c05
dsuh95 <d_suh95@yahoo.com>
2022-01-21 12:30:52 -0800

    Basic UI Complete

--- Antrashare_ui/src/app/login/login.component.ts
+++ Antrashare_ui/src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+    ngOnInit(): void {
+    };
+
+    SignIn() {
+        console.log('Sign In')
+    }
+
+    NeedHelp() {
+        console.log('Need Help')
+    }
+
+    SignUp() {
+        console.log('Sign Up')
+    }
+
+    LearnMore() {
+        console.log('Learn More')
+    }
 }
