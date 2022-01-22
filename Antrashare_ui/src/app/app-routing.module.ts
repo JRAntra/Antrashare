ca7b22b1dc10a0cb3a56fbea4b357a59654326fb
porry <porrychen@gmail.com>
2022-01-22 16:47:08 -0600

    Update app-routing.module.ts

--- Antrashare_ui/src/app/app-routing.module.ts
+++ Antrashare_ui/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
 
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { NewsFeedTabComponent } from './components/news-feed-tab/news-feed-tab.component';
 import { ProfileTabComponent } from './components/profile-tab/profile-tab.component';
@@ -9,9 +9,9 @@ import { SettingsTabComponent } from './components/settings-tab/settings-tab.com
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  { path: 'profile', component: ProfileTabComponent}, 
-  { path: 'newsfeed', component: NewsFeedTabComponent}, 
-  { path: 'settings', component: SettingsTabComponent},
+  { path: 'profile', component: ProfileTabComponent },
+  { path: 'newsfeed', component: NewsFeedTabComponent },
+  { path: 'settings', component: SettingsTabComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: ErrorPageComponent }
 ];
