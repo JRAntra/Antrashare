54232c1d30e3306ad2b780ab79b9be7a07497d14
wuyuxuan11998 <45754598+wuyuxuan11998@users.noreply.github.com>
2022-01-23 18:34:52 -0500

    add hide-element directives, add other page structure and router

--- Antrashare_ui/src/app/app.module.ts
+++ Antrashare_ui/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginWindowComponent } from './login-window/login-window.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginWindowComponent } from './components/login-window/login-window.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,11 +12,17 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HideElementDirective } from './directives/hide-element/hide-element.directive';
+import { MyProfilePageComponent } from './components/my-profile-page/my-profile-page/my-profile-page.component';
+import { NewsFeedComponent } from './components/news-feed/news-feed/news-feed.component';
+import { SettingsComponent } from './components/settings/settings/settings.component';
+import { StoryComponent } from './components/story/story/story.component';
+import { TimeoutDialogComponent } from './components/timeout-dialog/timeout-dialog/timeout-dialog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, LoginWindowComponent, LoginFormComponent
+    AppComponent, LoginWindowComponent, LoginFormComponent, HideElementDirective, MyProfilePageComponent, NewsFeedComponent, SettingsComponent, StoryComponent, TimeoutDialogComponent
   ],
   imports: [
     BrowserModule,
