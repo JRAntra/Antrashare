5ab1094887a05cb9b9bded8e4d111a253d97964d
xuhuarzhen <49420790+Xuhuarzhen@users.noreply.github.com>
2022-01-25 03:46:27 -0800

    feature/GetHired/HuazhenXu: implement profile page

--- Antrashare_ui/src/app/components/my-profile-page/my-profile-page.component.ts
+++ Antrashare_ui/src/app/components/my-profile-page/my-profile-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile-page',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile-page.component.scss']
 })
 export class MyProfilePageComponent implements OnInit {
-
+  isEditInfo: boolean = false;
+  
+  public userInfoFormGroup = new FormGroup({
+    userFnameFormControl: new FormControl('', Validators.required),
+    userLnameFormControl: new FormControl('', Validators.required),
+    userEmailFormControl: new FormControl('',  Validators.required),
+    userBDFormControl: new FormControl('')
+  })
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  OnFileChanged(event: any) {
+    const file: File = event.target.files[0];
+    console.log(file.name);
+  }
+  
+  triggerInput() {
+    console.log("save");
+  }
+  
+  SaveInfo() {
+    this.isEditInfo = false;
+  }
+  
+  EditInfo() {
+    this.isEditInfo = true;
+  }
 }
