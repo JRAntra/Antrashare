0f34755fc2578c0d1eaa3b8838927c30f04d2b85
Julian Cai <zcai57@wisc.edu>
2022-01-24 23:26:44 -0600

    add comment

--- Antrashare_ui/src/app/components/news-feed-tab/story-list/comment-list/comment-list.component.ts
+++ Antrashare_ui/src/app/components/news-feed-tab/story-list/comment-list/comment-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-comment-list',
   templateUrl: './comment-list.component.html',
   styleUrls: ['./comment-list.component.scss']
 })
-export class CommentListComponent implements OnInit {
+export class CommentListComponent implements OnInit, OnChanges {
 
+  @Input() userInput!: String;
+  @Output() cmtNumChange = new EventEmitter<number>();
+
+
+  cmtList: String[] = []; 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      if(this.userInput !== undefined){
+        // console.log(changes['userInput'].currentValue);
+        this.cmtList.push(this.userInput);
+      }
+      console.log("onChange" + this.cmtList + this.cmtList.length);
+      this.cmtNumChange.emit(this.cmtList.length);
+  }
+
+
+
 
 }
--- Antrashare_ui/src/app/components/news-feed-tab/story-list/story/story.component.ts
+++ Antrashare_ui/src/app/components/news-feed-tab/story-list/story/story.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-story',
@@ -8,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class StoryComponent implements OnInit {
 
   constructor() { }
-
+  userInput!: String;
+  cmtForm = new FormControl('');
   cmtStatus: boolean =  false;
+  cmtCount: number = 0;
   likes: number = 0;
   tempText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
  
@@ -24,4 +27,16 @@ export class StoryComponent implements OnInit {
   MoreLikes(){
     this.likes ++;
   }
+
+  onEnter(){
+    // console.log(this.cmtForm.value);
+    this.userInput = this.cmtForm.value;
+    // console.log(this.userInput);
+  }
+
+  cmtNumber(cmt: number){
+    console.log(cmt);
+    this.cmtCount = cmt;
+  }
+
 }
