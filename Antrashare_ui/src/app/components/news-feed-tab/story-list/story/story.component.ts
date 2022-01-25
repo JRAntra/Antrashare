063be05a12e10e7d9c34413ab5082d980fdf5b34
porry <porrychen@gmail.com>
2022-01-25 00:51:35 -0600

    Update story.component.ts

--- Antrashare_ui/src/app/components/news-feed-tab/story-list/story/story.component.ts
+++ Antrashare_ui/src/app/components/news-feed-tab/story-list/story/story.component.ts
@@ -7,34 +7,34 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./story.component.scss']
 })
 export class StoryComponent implements OnInit {
-
-  constructor() { }
   userInput!: String;
   cmtForm = new FormControl('');
-  cmtStatus: boolean =  false;
+  cmtStatus: boolean = false;
   cmtCount: number = 0;
   likes: number = 0;
   tempText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
- 
+
+  constructor() { }
+
   ngOnInit(): void {
   }
 
-  showComment(){
+  showComment() {
     // console.log(this.cmtStatus);
     this.cmtStatus = !this.cmtStatus;
   }
 
-  MoreLikes(){
-    this.likes ++;
+  MoreLikes() {
+    this.likes++;
   }
 
-  onEnter(){
+  onEnter() {
     // console.log(this.cmtForm.value);
     this.userInput = this.cmtForm.value;
     // console.log(this.userInput);
   }
 
-  cmtNumber(cmt: number){
+  cmtNumber(cmt: number) {
     console.log(cmt);
     this.cmtCount = cmt;
   }
