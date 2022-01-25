02b990f6055adeff94c493b4fe8c545fcec732d2
porry <porrychen@gmail.com>
2022-01-25 00:30:04 -0600

    added toolbar in the app component page

--- Antrashare_ui/src/app/app.component.ts
+++ Antrashare_ui/src/app/app.component.ts
@@ -9,18 +9,22 @@ import { Router, NavigationEnd } from '@angular/router';
 export class AppComponent {
   title = 'Antrashare_ui';
   tabs = [
-    {name: 'My Profile', path: 'profile'},
-    {name: 'News Feed', path: 'newsfeed'},
-    {name: 'Settings', path: 'settings'}
+    { name: 'My Profile', path: 'profile' },
+    { name: 'News Feed', path: 'newsfeed' },
+    { name: 'Settings', path: 'settings' }
   ];
 
-  currentRoute: string = '';
+  private currentRoute: string = '';
+
+  get isOpen(): boolean {
+    return this.currentRoute != '/login' && this.currentRoute != '/';
+  }
 
   constructor(private router: Router) {
-    router.events.subscribe(event => {
+    this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.currentRoute = event.url;
       }
-    });;
-  };
+    });
+  }
 }
