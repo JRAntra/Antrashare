e594d20540664fbf53639be6128870b9853065e3
Karo Goumrouyan <63257431+karo115@users.noreply.github.com>
2022-01-25 01:03:59 -0800

    Created log out button on settings page

--- Antrashare_ui/src/app/components/settings-tab/settings-tab.component.ts
+++ Antrashare_ui/src/app/components/settings-tab/settings-tab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-settings-tab',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsTabComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logOut(){
+    console.log("Logged out")
+    this.router.navigate(['login'])
   }
 
 }
