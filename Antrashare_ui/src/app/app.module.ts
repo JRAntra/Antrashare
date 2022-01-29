f7938c7d89ddc6224189f0054f83ad89c35142c0
porry <porrychen@gmail.com>
2022-01-29 01:26:06 -0600

    create the timeout dialog component

--- Antrashare_ui/src/app/app.module.ts
+++ Antrashare_ui/src/app/app.module.ts
@@ -26,11 +26,12 @@ import { Error404Component } from './components/error-page/error404/error404.com
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { LoginFormComponent } from './components/login-page/login-form/login-form.component';
 import { NewsFeedTabComponent } from './components/news-feed-tab/news-feed-tab.component';
+import { ProfileFormComponent } from './components/profile-tab/profile-form/profile-form.component';
+import { ProfileTabComponent } from './components/profile-tab/profile-tab.component';
 import { SettingsTabComponent } from './components/settings-tab/settings-tab.component';
 import { StoryComponent } from './components/news-feed-tab/story-list/story/story.component';
 import { StoryListComponent } from './components/news-feed-tab/story-list/story-list.component';
-import { ProfileFormComponent } from './components/profile-tab/profile-form/profile-form.component';
-import { ProfileTabComponent } from './components/profile-tab/profile-tab.component';
+import { TimeoutComponent } from './dialogs/timeout/timeout.dialog.component';
 
 @NgModule({
   declarations: [
@@ -44,11 +45,12 @@ import { ProfileTabComponent } from './components/profile-tab/profile-tab.compon
     LoginPageComponent,
     LoginFormComponent,
     NewsFeedTabComponent,
+    ProfileFormComponent,
+    ProfileTabComponent,
     SettingsTabComponent,
     StoryComponent,
     StoryListComponent,
-    ProfileFormComponent,
-    ProfileTabComponent,
+    TimeoutComponent,
   ],
   imports: [
     BrowserModule,
---
+++ Antrashare_ui/src/app/dialogs/timeout/timeout.dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeout',
+  templateUrl: './timeout.component.html',
+  styleUrls: ['../../css/dialog.component.scss']
+})
+export class TimeoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
