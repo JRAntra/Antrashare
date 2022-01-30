f8e27599b9a238c6a1fc9cfb0db0fcf5577d1d4f
karo115 <kgoumrouyan@gmail.com>
2022-01-30 06:07:41 -0800

    Added logout window and some css

--- Antrashare_ui/src/app/app-routing.module.ts
+++ Antrashare_ui/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LogoutWindowComponent } from './components/login-page/logout-window/logout-window.component';
 import { NewsFeedTabComponent } from './components/news-feed-tab/news-feed-tab.component';
 import { ProfileTabComponent } from './components/profile-tab/profile-tab.component';
 import { SettingsTabComponent } from './components/settings-tab/settings-tab.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileTabComponent },
   { path: 'newsfeed', component: NewsFeedTabComponent },
   { path: 'settings', component: SettingsTabComponent },
+  { path: 'logout', component: LogoutWindowComponent},
 
   { path: '**', component: ErrorPageComponent }
 ];
