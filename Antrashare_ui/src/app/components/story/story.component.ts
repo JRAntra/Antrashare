58b042534b28d7e50c7b048d87b11f386d638ac0
Daniel <d_suh95@yahoo.com>
2022-02-03 13:42:03 -0800

    Story edit for new news model

--- Antrashare_ui/src/app/components/story/story.component.ts
+++ Antrashare_ui/src/app/components/story/story.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { Story } from '../news-feed/news-feed.component';
-
+import { News } from 'src/app/models/newsfeed.models';
 @Component({
   selector: 'story',
   templateUrl: './story.component.html',
   styleUrls: ['./story.component.scss']
 })
 export class StoryComponent implements OnInit {
-  @Input() story!:Story
+  @Input() story!:News
   @Output() storyEmiter = new EventEmitter()
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.story.content.text);
   }
 
 }
