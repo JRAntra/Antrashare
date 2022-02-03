474a3b72f3fbfb053e997b47bb2335d5b70d7a0d
wuyuxuan11998 <45754598+wuyuxuan11998@users.noreply.github.com>
2022-02-03 15:13:54 -0500

    newsservice and newsfeed

--- Antrashare_ui/src/app/app.module.ts
+++ Antrashare_ui/src/app/app.module.ts
@@ -1,12 +1,8 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginWindowComponent } from './components/login-window/login-window.component';
-import { LoginFormComponent } from './components/login-window/login-form/login-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,18 +11,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog'
 import { ReactiveFormsModule } from '@angular/forms';
 
-
+import { AppComponent } from './app.component';
+import { BottomBarComponent } from './components/bottom/bottom-bar/bottom-bar.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HideElementDirective } from 'src/app/directives/hide-element/hide-element.directive';
+import { LoginWindowComponent } from './components/login-window/login-window.component';
+import { LoginFormComponent } from './components/login-window/login-form/login-form.component';
+import { LogoutWindowComponent } from './components/settings/logout-window/logout-window.component';
 import { MyProfilePageComponent } from './components/my-profile-page/my-profile-page.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { NewsService } from 'src/app/services/news/news.service';
 import { NewsFeedComponent } from './components/news-feed/news-feed.component';
 import { StoryComponent } from './components/story/story.component';
 import { TimeoutDialogComponent } from 'src/app/components/timeout-dialog/timeout-dialog.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavbarComponent } from './components/header/navbar/navbar.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
-import { BottomBarComponent } from './components/bottom/bottom-bar/bottom-bar.component';
-import { LogoutWindowComponent } from './components/settings/logout-window/logout-window.component';
 
 
 @NgModule({
@@ -50,6 +49,7 @@ import { LogoutWindowComponent } from './components/settings/logout-window/logou
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
@@ -58,7 +58,7 @@ import { LogoutWindowComponent } from './components/settings/logout-window/logou
     MatButtonModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [NewsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Antrashare_ui/src/app/components/news-feed/news-feed.component.ts
+++ Antrashare_ui/src/app/components/news-feed/news-feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news/news.service';
 
 @Component({
   selector: 'app-news-feed',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-feed.component.scss']
 })
 export class NewsFeedComponent implements OnInit {
+  /*
   story0: Story = {
     id: 0,
     cover: "story",
@@ -22,17 +24,16 @@ export class NewsFeedComponent implements OnInit {
     id: 0,
     cover: "story",
     content: "fasfasfa"
-  }
-  storyList: Story[] = [this.story0, this.story1, this.story2]
+  }*/
+  //storyList: Story[] = [this.story0, this.story1, this.story2]
 
-  constructor() { }
+  constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
+    this.newsService.getNews().subscribe(console.log)
   }
 
-  selectStory(story: Story) {
-
-  }
+  
 
 }
 
