e26194d5afbc3d1a6eccf9abc5e331fcd15b5ced
porry <porrychen@gmail.com>
2022-02-03 17:29:49 -0600

    build postFormGroup

--- Antrashare_ui/src/app/components/news-feed-tab/story-form/story-form.component.ts
+++ Antrashare_ui/src/app/components/news-feed-tab/story-form/story-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-story-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoryFormComponent implements OnInit {
 
-  constructor() { }
+  postForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.postForm = this.fb.group({
+      text: '',
+      image: this.fb.array([]),
+      video: this.fb.array([])
+    });
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  public post(): void {
+    console.log(this.postForm.value);
   }
 
 }
