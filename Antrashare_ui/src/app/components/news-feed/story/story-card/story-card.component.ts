833db61005908427f1333d35049129d7426fc97b
zk39 <811095332@qq.com>
2022-02-04 05:11:27 -0500

    1.Fix previous log out button theme.scss can not be applied' bug

--- Antrashare_ui/src/app/components/news-feed/story/story-card/story-card.component.ts
+++ Antrashare_ui/src/app/components/news-feed/story/story-card/story-card.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { story } from 'src/app/models/user.models';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from 'src/app/services/newsfeed/data.service';
 @Component({
   selector: 'app-story-card',
   templateUrl: './story-card.component.html',
   styleUrls: ['./story-card.component.scss']
 })
 export class StoryCardComponent implements OnInit {
+  storyList: story[] = [];
+  dataFromServer: any;
 
-  constructor() { }
+  constructor(
+    private dataService: DataService,
+    private _httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
-  }
+    //this.dataService.getNewsFeed().subscribe((data) => (this.storyList = data));
 
+    this.dataService.getNewsFeed().subscribe((data) => {
+      this.dataFromServer = data;
+
+      console.log(this.dataFromServer);
+
+      this.storyList = this.dataFromServer;
+
+      console.log(this.storyList);
+    });
+  }
 }
