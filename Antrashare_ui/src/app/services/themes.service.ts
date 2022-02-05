4c495bddc1d32b463f0bc7b46e317020eb0437bf
porry <porrychen@gmail.com>
2022-02-05 10:27:45 -0600

    Update themes.service.ts

--- Antrashare_ui/src/app/services/themes.service.ts
+++ Antrashare_ui/src/app/services/themes.service.ts
@@ -10,9 +10,21 @@ import { ThemeType } from '../models/theme.model';
 export class ThemesService {
   private activeTheme$ = new BehaviorSubject(APP_CONFIG.defaultTheme);
 
-  constructor(public overlayContainer: OverlayContainer) { this.setOverlayContainer(APP_CONFIG.defaultTheme) }
+  /**
+   * Constructor
+   */
+  constructor(public overlayContainer: OverlayContainer) {
+    // Set default theme
+    this.setOverlayContainer(APP_CONFIG.defaultTheme);
+  }
 
-  private setOverlayContainer(theme: string): void {
+  /**
+   * Set OverlayContainer: set activeTheme class to classList
+   *
+   * @param theme
+   * @private
+   */
+  private setOverlayContainer(theme: ThemeType): void {
     const overlayContainerClassList = this.overlayContainer.getContainerElement().classList;
     const needToRemoveClass = Array.from(overlayContainerClassList).filter((containerClass: string) => containerClass.includes('-theme'));
     if (needToRemoveClass.length) {
@@ -21,11 +33,20 @@ export class ThemesService {
     overlayContainerClassList.add(theme);
   }
 
+  /**
+   * Set activeTheme
+   *
+   * @param theme
+   */
   setActiveTheme(theme: ThemeType = APP_CONFIG.defaultTheme): void {
     this.setOverlayContainer(theme);
     this.activeTheme$.next(theme);
   }
 
+  /**
+   * Get activeTheme
+   *
+   */
   getActiveTheme(): Observable<ThemeType> {
     return this.activeTheme$.asObservable();
   }
