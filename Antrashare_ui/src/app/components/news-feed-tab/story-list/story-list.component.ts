d52544d29836f93d015684baa1d9a9f914b92cba
porry <porrychen@gmail.com>
2022-02-06 23:29:47 -0600

    Update story-list.component.ts

--- Antrashare_ui/src/app/components/news-feed-tab/story-list/story-list.component.ts
+++ Antrashare_ui/src/app/components/news-feed-tab/story-list/story-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { News } from 'src/app/models/newsfeed.model';
 import { NewsFeedService } from 'src/app/services/news-feed.service';
 
@@ -8,22 +8,26 @@ import { NewsFeedService } from 'src/app/services/news-feed.service';
   templateUrl: './story-list.component.html',
   styleUrls: ['../../../css/story-list.component.scss']
 })
-export class StoryListComponent implements OnInit {
+export class StoryListComponent implements OnInit, OnDestroy {
 
-  totalNews: News[] = [];
+  private newsSubjection!: Subscription;
 
   totalNews$!: Observable<News[]>;
 
   constructor(private NewsData: NewsFeedService) { }
 
+  ngOnDestroy(): void {
+    this.newsSubjection.unsubscribe();
+  }
+
   ngOnInit(): void {
+    this.totalNews$ = this.NewsData.getList();
+
     this.getAllNews();
   }
 
-
-  getAllNews() {
-    this.totalNews$ = this.NewsData.getList();
-    this.NewsData.get().subscribe();
+  getAllNews(): void {
+    this.newsSubjection = this.NewsData.get().subscribe();
   };
 
 }
