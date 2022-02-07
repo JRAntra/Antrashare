a151e6a087f0267b0b3eb6a1a07a41c579825b1f
xuhuarzhen <49420790+Xuhuarzhen@users.noreply.github.com>
2022-02-07 05:58:50 -0800

    create comment dialog & display all comments of the selected story && create add new comment section in dialog && modify posting new feed method && need to figure out how to post new comments by url plus id

--- Antrashare_ui/src/app/components/news-feed/comment-dialog/comment-dialog.component.ts
+++ Antrashare_ui/src/app/components/news-feed/comment-dialog/comment-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 import { News } from 'src/app/models/newsfeed.models';
+import { NewsService } from 'src/app/services/news/news.service';
 
 @Component({
   selector: 'app-comment-dialog',
@@ -8,15 +9,19 @@ import { News } from 'src/app/models/newsfeed.models';
   styleUrls: ['./comment-dialog.component.scss']
 })
 export class CommentDialogComponent implements OnInit {
-  @Input() commentList!: News["comment"];
+  commentList: any;
   
   constructor(
-    @Inject(MAT_DIALOG_DATA) data: any,
+    private newsService: NewsService,
+    @Inject(MAT_DIALOG_DATA) private data: { 
+      story: News, //story_id: string
+    },
     private dialogRef: MatDialogRef<CommentDialogComponent>
   ) { }
   
   ngOnInit(): void {
-    
+    this.commentList = this.data.story.comment;
+    // this.newsService.getNewsById(this.data.story_id).subscribe(story => this.commentList = story.comment);
   }
 
 }
