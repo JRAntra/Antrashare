adb63de149f0c95ba4bf36c794c4a4595681e059
Daniel <d_suh95@yahoo.com>
2022-02-08 11:38:09 -0800

    Implemented basic image url validation on story component

--- Antrashare_ui/src/app/components/news-feed/story/story.component.ts
+++ Antrashare_ui/src/app/components/news-feed/story/story.component.ts
@@ -35,12 +35,8 @@ export class StoryComponent implements OnInit {
 
     //video link validation
     this.validateVideoUrl();
-    //image presence validation. Error (i.e. bad link) is handled in HTML.
-    if (this.imageLink !== '') {
-      this.hasImage = true;
-    } else {
-      this.hasImage = false;
-    }
+    //image url validation. Error (i.e. bad link) is also handled in HTML.
+    this.validateImageUrl();
 
     //link sanitizers
     this.safeVideo = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoLink);
@@ -57,6 +53,19 @@ export class StoryComponent implements OnInit {
       this.textboxSize = {
         'grid-template-rows': '55% auto'
       };
+    }
+  }
+
+  validateImageUrl() {
+    if (this.videoLink !== undefined || this.videoLink !== '') {
+      var urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+      var isValidImage = this.imageLink.match(urlRegex);
+      if (isValidImage) {
+        this.hasImage = true;
+      }
+      else {
+        this.hasImage = false;
+      }
     }
   }
 
