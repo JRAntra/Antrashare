023e7c7cc1902c4a609788c8d27c615b913ee115
FeMiqui <fellipemiqui@gmail.com>
2022-02-09 15:40:32 -0500

    fixing some issues

--- Antrashare_ui/src/app/services/login/login-service.service.ts
+++ Antrashare_ui/src/app/services/login/login-service.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { baseUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  private loginUrl = [baseUrl,].join();
   constructor(private http: HttpClient) { }
 
   getUserAccount() {
