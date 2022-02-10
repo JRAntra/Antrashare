34c4e199f10f5e4d9e5d15c476e9bc6ac3dbe734
porry <porrychen@gmail.com>
2022-02-10 10:43:34 -0600

    add login router

---
+++ Antrashare_server/routes/login.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+
+const { Profile } = require('../models/profile');
+
+router.get("/", async (req, res) => {
+  const users = await Profile.find();
+  res.send(users);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  let user = await Profile.findOne({ userEmail: req.body.userEmail });
+  if (!user) {
+    return res.status(400).send('Cannot find this email.');
+  }
+
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  ); // compare password;
+  if (!validPassword) {
+    return res.status(400).send('Invalid email or password.');
+  }
+
+  const token = Profile.generateAuthToken.call(user);
+  console.log(token);
+
+  const infoBack = {
+    name: user.name,
+    userName: user.userName,
+    userEmail: user.userEmail,
+
+    userRole: user.userRole,
+    age: user.age,
+    gender: user.gender,
+    phone: user.phone,
+
+    bearerToken: token,
+  }
+
+  res.header('bearerToken', token).send(infoBack);
+});
+
+const validate = (user) => {
+  const schema = Joi.object({
+    userEmail: Joi.string().min(3).max(50).required(),
+    password: Joi.string().min(3).max(50).required(),
+  });
+  return schema.validate(user);
+}
+
+module.exports = router;
--- Antrashare_server/startup/routes.js
+++ Antrashare_server/startup/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const news = require('../routes/news');
+const login = require('../routes/login');
 
 module.exports = function(app) {
   app.use(express.json());
   app.use(cors());
 
   // models router
+  app.use('/api/login', login);
   app.use('/api/news', news);
 }
