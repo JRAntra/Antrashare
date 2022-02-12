7e5e477f6c2cad0f75b49d503f0793c99c5e0472
porry <porrychen@gmail.com>
2022-02-12 14:45:58 -0600

    Update app.config.ts

--- Antrashare_ui/src/app/core/config/app.config.ts
+++ Antrashare_ui/src/app/core/config/app.config.ts
@@ -4,17 +4,18 @@ export const APP_CONFIG = {
   admin: 'admin',
 
   defaultIdleStatus: {
-    idleTime: 10 * 60, // 10 minutes
-    timeoutTime: 10 // 10 seconds
+    idleTime: 10 * 60,  // 10 minutes
+    timeoutTime: 10     // 10 seconds
   },
   defaultStory: {
     numImages: 1,
-    numVideos: 1
+    numVideos: 1,
+    pageSize: 10
   },
   defaultTheme: ThemeType.Dark,
 
   localStorage: {
-    prefix: 'TBD-', // Team Best DEVs
+    prefix: 'TBD-',     // Team Best DEVs
     theme: 'theme',
     idle: 'idle',
     token: 'bearerToken'
