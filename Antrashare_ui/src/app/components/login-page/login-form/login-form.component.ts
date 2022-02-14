9aa559f86683aaf26ee2ae0e131a155e4257c283
porry <porrychen@gmail.com>
2022-02-14 16:20:37 -0600

    test asyc validator

--- Antrashare_ui/src/app/components/login-page/login-form/login-form.component.ts
+++ Antrashare_ui/src/app/components/login-page/login-form/login-form.component.ts
@@ -3,28 +3,35 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, AsyncValidatorFn } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
-import { catchError, debounceTime, of } from 'rxjs';
+import { catchError, debounceTime, map, of, switchMap, tap } from 'rxjs';
 // import { access } from 'fs';
 import { UserAccount } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { Observable } from 'rxjs';
 
-const asyncValidator = (HttpClient: HttpClient) => (control: AbstractControl): Observable<ValidationErrors> | null => {
-  control.valueChanges.pipe(debounceTime(1000)).subscribe((email)=>{
-    return HttpClient.get("http://localhost:4231/api/register/checkExistByEmail/" + email).subscribe(
-      (response) =>{
-        console.log(email);
-        console.log(response);
-      }
-      // (catchError)
-     );
-    
-    // return HttpClient.get("http://localhost:4231/api/register/checkExistByEmail/" + email);
-  })
-  
-  return null;
-}
+const asyncValidator = (httpClient: HttpClient): AsyncValidatorFn => {
 
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    // return httpClient.get("http://localhost:4231/api/register/checkExistByEmail/" + control.value).pipe(
+    //         debounceTime(1000),
+    //         map((data: any) => {
+    //           console.log(data);
+    //           return data ?? { "exist": true };
+    //         })
+    //       );
+    // console.log(httpClient);
+    return control.valueChanges.pipe(
+      debounceTime(1000),
+      switchMap(() => {
+        return httpClient.get("http://localhost:4231/api/register/checkExistByEmail/" + control.value);
+      }),
+      map((data: any) => {
+        console.log(data);
+        return data ?? { "userEmailExist": false };
+      })
+    );
+  }
+}
 
 @Component({
   selector: 'app-login-form',
@@ -36,9 +43,8 @@ export class LoginFormComponent implements OnInit {
   userLoginForm = new FormGroup({
     email: new FormControl('', [
       Validators.required,
-      Validators.minLength(5),
-      asyncValidator(this.http),
-    ]),
+      Validators.minLength(5),],
+      [asyncValidator(this.http)]),
     password: new FormControl('', [
       Validators.required,
       Validators.minLength(5),
@@ -51,7 +57,7 @@ export class LoginFormComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
-    private http: HttpClient
+    public http: HttpClient
   ) { }
 
   ngOnInit(): void { }
@@ -67,14 +73,14 @@ export class LoginFormComponent implements OnInit {
       userEmail: this.userLoginForm.get('email')?.value,
       password: this.userLoginForm.get('password')?.value
     }
-    
+
     this.authService.login(account).subscribe(
       () => {
         const redirectUrl = this.activatedRoute.snapshot.queryParamMap.get('redirectTo') || 'newsfeed';
         this.router.navigateByUrl(redirectUrl);
         // console.log(this.authService.userName);
         // console.log(this.authService.userRole);
-      }, 
+      },
       (response) => {
         this.userLoginForm.enable();
       }
@@ -82,7 +88,7 @@ export class LoginFormComponent implements OnInit {
 
     //placeholder
     console.log(this.userLoginForm.value);
-    
+
   }
 
   getErrorMessageEmail() {
@@ -131,9 +137,9 @@ export class LoginFormComponent implements OnInit {
     return { capLetter: { value: control.value } };
   }
 
-  getValidate(): any{
-    if( this.userLoginForm.get('email')?.hasError('registered')){
-  
+  getValidate(): any {
+    if (this.userLoginForm.get('email')?.hasError('registered')) {
+
       return "Email has been registered";
     }
     return 'Email is OK to use';
