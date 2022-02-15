214b50a5222dea94204c65efd2200eb45538adf2
FeMiqui <fellipemiqui@gmail.com>
2022-02-15 13:41:36 -0500

    changed so the profile displays username, email and the posts he/she made

--- Antrashare_ui/src/app/components/my-profile/my-profile.component.ts
+++ Antrashare_ui/src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { newsStory } from 'src/app/models/newsStory.models';
+import { newsFeedService } from 'src/app/services/newsfeed/newsfeed.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.scss']
 })
 export class MyProfileComponent implements OnInit {
+  public userName: string = "";
+  public userEmail: string = "";
+  public avatar_url: string = "";
+  public storyList: newsStory[] = [];
 
-  constructor() { }
+  constructor(private authService: AuthService, private profileNews: newsFeedService) { }
 
   ngOnInit(): void {
+    this.userName = this.authService.getUserName();
+    this.userEmail = this.authService.getUserEmail();
+    this.profileNews.getNewsFeed().subscribe((data) => {
+      this.storyList = data.filter((element) => element.publisherName === this.userName);
+    })
   }
-
 }
