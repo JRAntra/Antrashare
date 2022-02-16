07a4322677ed2a7cba0edce34bd89f2b18405c7d
porry <porrychen@gmail.com>
2022-02-16 01:24:56 -0600

    Update auth.guard.spec.ts

--- Antrashare_ui/src/app/core/guards/auth.guard.spec.ts
+++ Antrashare_ui/src/app/core/guards/auth.guard.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoginPageComponent } from 'src/app/components/login-page/login-page.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 import { AuthGuard } from './auth.guard';
 
-describe('AuthGuard', () => {
-  let service: AuthGuard;
+fdescribe('AuthGuard', () => {
+  let authGuard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthGuard);
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([{ path: 'login', component: LoginPageComponent }]), HttpClientTestingModule],
+      providers: [AuthService]
+    });
+    authGuard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authGuard).toBeTruthy();
+  });
+
+  it('should return isAuthenticated', () => {
+    authGuard.canActivateChild(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'login' }).
+      subscribe((canActive) => {
+        expect(canActive).toBeTruthy();
+      });
   });
 });
