541f133299f6f2d5312d415220bbb8c3e8929605
FeMiqui <fellipemiqui@gmail.com>
2022-02-16 15:23:53 -0500

    Commenting the table until we fix the errors and adding some styling to the comments

--- Antrashare_ui/src/app/components/news-feed/story/comment-list/comment-list.component.ts
+++ Antrashare_ui/src/app/components/news-feed/story/comment-list/comment-list.component.ts
@@ -4,9 +4,6 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { NewsFeedComment } from 'src/app/models/comments.models';
-import { newsFeedService } from 'src/app/services/newsfeed/newsfeed.service';
-import { NewsFeedComponent } from '../../news-feed.component';
-
 
 @Component({
   selector: 'app-comment-list',
@@ -14,44 +11,39 @@ import { NewsFeedComponent } from '../../news-feed.component';
   styleUrls: ['./comment-list.component.scss']
 })
 
-
-
 export class CommentListComponent implements OnInit {
   public commentsList: any;
   public storyId!: string;
-  public currentItemsToShow :any ;
+  public currentItemsToShow: any;
   public Data: NewsFeedComment[] = [];
   constructor(@Inject(MAT_DIALOG_DATA) public storyData: any) {
-   
+
   }
-  
-
-  
-
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   obs!: Observable<any>;
   dataSource: MatTableDataSource<NewsFeedComment> = new MatTableDataSource<NewsFeedComment>(this.Data);
 
-  
+
   ngOnInit(): void {
-    
+
     this.commentsList = this.storyData.storyComments;
     this.storyId = this.storyData.storyId;
     this.dataSource.paginator = this.paginator;
     this.addData();
     console.log(this.dataSource.data);
   }
-  
+
   onPageChange($event: { pageIndex: number; pageSize: number; }) {
-   // this.currentItemsToShow =  this.items.slice($event.pageIndex*$event.pageSize, $event.pageIndex*$event.pageSize + $event.pageSize);
-  
+    console.log($event);
+    // this.currentItemsToShow =  this.items.slice($event.pageIndex*$event.pageSize, $event.pageIndex*$event.pageSize + $event.pageSize);
+
   }
-  addData(): void{
-      this.commentsList.forEach((element: any) => {
+  addData(): void {
+    this.commentsList.forEach((element: any) => {
       this.Data.push(element);
       //console.log(element);
     });
-  
+
   }
 }
