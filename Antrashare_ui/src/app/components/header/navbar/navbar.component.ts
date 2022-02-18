76d135eb9adc51f0198fb3c465553ee90b613549
xuhuarzhen <49420790+Xuhuarzhen@users.noreply.github.com>
2022-02-18 01:57:34 -0800

    optimized some components & add a fail dialog for auth check

--- Antrashare_ui/src/app/components/header/navbar/navbar.component.ts
+++ Antrashare_ui/src/app/components/header/navbar/navbar.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { UserProfile } from 'src/app/models/user.models';
 import { CacheService } from 'src/app/services/cache.service';
+import { LoginService } from 'src/app/services/login/login.service';
+import { LogoutDialogComponent } from '../../logout-dialog/logout-dialog.component';
 
 @Component({
   selector: 'app-navbar',
@@ -10,16 +14,26 @@ import { CacheService } from 'src/app/services/cache.service';
 export class NavbarComponent implements OnInit {
   userInfo!: UserProfile | null;
   role: string = "Admin/User";
-  constructor(private cacheService: CacheService) { }
+  isLogIn: boolean = false;
+  dialogRef?: MatDialogRef<LogoutDialogComponent>;
+  
+  constructor(
+    private router: Router,
+    private cacheService: CacheService,
+    private loginService: LoginService,
+    private dialog: MatDialog) {
+    this.loginService.tokenInfo$.subscribe(res =>
+      this.isLogIn = res ? true : false
+    )
+  }
 
   ngOnInit(): void {
-    
   }
-  
+
   onTestRole() {
     const isSignIn = this.cacheService.isLogin;
     this.userInfo = this.cacheService.getUserInfo();
-    if(isSignIn) {
+    if (isSignIn) {
       if (this.userInfo?.userRole === "user") {
         this.userInfo.userRole = "admin";
         this.role = "user";
@@ -33,6 +47,14 @@ export class NavbarComponent implements OnInit {
     }
     else {
       console.log("didnt sign in !")
+    }
+  }
+  
+  openLogoutDialog() {
+    if (this.isLogIn) {
+      this.dialogRef = this.dialog.open(LogoutDialogComponent);
+    } else {
+      this.router.navigate(['/login']);
     }
   }
 }
